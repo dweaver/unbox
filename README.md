f8d9568791326e3edbe72e70d3bc82dc56114110
Dan Weaver <danweaver@exosite.com>
2015-02-13 10:20:30 -0600

    hello world website

---
+++ README.md
@@ -0,0 +1,12 @@
+# Unbox (beta)
+
+Unbox is an example of a customer registration site for [Exosite](https://exosite.com)-connected products.
+
+## Setup
+
+```
+$ 
+$ npm install
+```
+
+Then go to https://localhost:8080.
---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 4040));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
