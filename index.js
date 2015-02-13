94ce5f0a14022f4b40a857d233c8d007546543f3
Dan Weaver <danweaver@exosite.com>
2015-02-13 16:21:54 -0600

    list models

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 'use strict';
 var express = require('express');
+var _ = require('underscore');
+var cons = require('consolidate');
 var app = express();
+var provision = require('./provision');
 
 app.set('port', (process.env.PORT || 4040));
 app.use(express.static(__dirname + '/public'));
 
+// use underscore for templates
+app.engine('html', cons.underscore);
+app.set('views', './views');
+app.set('view engine', 'html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  provision.listModels(process.env.VENDOR_TOKEN, function(err, models) {
+    if (err) {
+      console.log(err);
+      response.write('Error: ' + err);
+      return;
+    }
+    response.render('index.html', {models: models});
+  });
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
---
+++ provision.js
@@ -0,0 +1,43 @@
+/**
+ * provision API endpoints
+ */
+'use strict';
+
+var request = require('request');
+var _ = require('underscore');
+
+var OPTIONS = {
+  host: 'm2.exosite.com',
+  agent: 'node-onep',
+  https: false
+};
+
+/**
+ * Set options for provisioning
+ */
+exports.setOptions = function(options) {
+  _.extend(OPTIONS, options);
+};
+
+/**
+ * List models for a vendor
+ */
+exports.listModels = function(token, callback) {
+  var options = {
+    url: (OPTIONS.https ? 'https://' : 'http://') + OPTIONS.host + '/provision/manage/model/',
+    headers: {
+      'User-Agent': OPTIONS.agent,
+      'X-Exosite-Token': token
+    }
+  };
+  request(options, function (err, response, body) {
+    if (err) {
+      return callback(err);
+    }
+    if (response.statusCode >= 300) {
+      return callback(response.statusCode);
+    }
+    var models = body.trim().split('\n');
+    callback(null, models);
+  });
+};
