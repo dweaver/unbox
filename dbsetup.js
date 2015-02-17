4ed49837b0d78d4bfe777a0e7e63f57738087de8
Dan Weaver <danweaver@exosite.com>
2015-02-17 14:11:39 -0600

    clean up debug messages

--- dbsetup.js
+++ dbsetup.js
@@ -1,10 +1,7 @@
 var db = require('./db');
 
-console.log('got here 1');
 db.connect(function(err) {
-  console.log('got here 2');
   db.setup(function(err) {
-    console.log('got here 3');
     if (err) {
       console.log(err);
       process.exit(1);
