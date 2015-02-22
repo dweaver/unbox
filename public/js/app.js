56eec0854b86aa4b36d9d437d1eb391eb4781da2
Dan Weaver <danweaver@exosite.com>
2015-02-22 15:22:53 -0600

    remove demo modal

--- public/js/app.js
+++ public/js/app.js
@@ -145,43 +145,6 @@
       });
     };
   })
-  .controller('DemoModalCtrl', function ($scope, $modal, $log) {
-    $scope.items = ['item1', 'item2', 'item3'];
-
-    $scope.open = function (size) {
-
-      var modalInstance = $modal.open({
-        templateUrl: 'demoModalContent.html',
-        controller: 'DemoModalInstanceCtrl',
-        size: size,
-        resolve: {
-          items: function () {
-            return $scope.items;
-          }
-        }
-      });
-
-      modalInstance.result.then(function (selectedItem) {
-        $scope.selected = selectedItem;
-      }, function () {
-        $log.info('Modal dismissed at: ' + new Date());
-      });
-    };
-  })
-  .controller('DemoModalInstanceCtrl', function ($scope, $modalInstance, items) {
-    $scope.items = items;
-    $scope.selected = {
-      item: $scope.items[0]
-    };
-
-    $scope.ok = function () {
-      $modalInstance.close($scope.selected.item);
-    };
-
-    $scope.cancel = function () {
-      $modalInstance.dismiss('cancel');
-    };
-  })
   .controller('AddProductModalInstanceCtrl', function ($scope, $modalInstance, models, products) {
     $scope.models = models;
     $scope.products = products;
